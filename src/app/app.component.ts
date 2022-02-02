9afbb8d30729227ba0ead16e65c3b1feffeb88bd
Ricardo <ricardo.thomas@medicalalert.com>
2022-02-02 18:48:54 -0500

    adding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { StatusService } from './shared/status.service';
+import {MbiObj} from './Mbi';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'CodeChallenge';
+  generatedMbi: string = '';
+  obj:MbiObj = new MbiObj('');
+  isValid: boolean = false;
+
+  constructor(private statusService: StatusService) { }
+
+  ngOnInit() { }
+
+  onSubmit() { 
+    this.statusService
+    .postMBI(this.obj)
+    .then((res: any) => {
+      console.log('verify mbi -> ', res);
+      this.isValid = res.result;
+    }) 
+  }
+
+  getGeneratedMbi(){
+    this.statusService
+    .getMBI()
+    .then((result: any) => {
+      console.log('testing ->', result);
+      this.generatedMbi = result.mbi;
+    });
+  }
 }
